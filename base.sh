a55d0bc80d5ca7dd8f37c1251af6b04e864ddb4c
mjwhitehurst <matt.whitehursts@gmail.com>
2022-08-27 13:09:35 +0100

    small start

--- base.sh
+++ base.sh
@@ -3,3 +3,19 @@
 #############################################
 
 export MATTS_CONFIG="ENABLED"
+export CONFIG_DIR=$HOME/config
+
+
+
+
+
+if [[ ! -d $CONFIG_DIR ]]; then
+    echo "config dir missing! all config stuff needs to be in \$HOME/config"
+else
+    if [[ -f $CONFIG_DIR/script_funcs.sh ]]; then
+        source $CONFIG_DIR/script_funcs.sh
+    else
+        echo "script funcs missing :("
+    fi
+fi #if config dir exists
+
